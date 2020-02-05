69469615efb2e2a588be6d345e74c597bb6c116b
naccaratocarolina <naccaratocarolina@poli.ufrj.br>
2020-02-05 12:35:20 -0200

    add guard

---
+++ aulas-particulares-app/src/app/guards/Auth/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router) { }
+
+  canActivate():Observable<boolean> | Promise<boolean> | boolean {
+  	/**
+  	return this.router.navigateByUrl('/inicial');
+  	**/
+  	return;
+  }
+
+}
--- aulas-particulares-app/src/app/tabs/tabs-routing.module.ts
+++ aulas-particulares-app/src/app/tabs/tabs-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
+import { AuthGuard } from '../guards/Auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -13,7 +14,8 @@ const routes: Routes = [
           {
             path: '',
             loadChildren: () =>
-              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+              import('../tab1/tab1.module').then(m => m.Tab1PageModule),
+              canActivate: [AuthGuard]
           }
         ]
       },
