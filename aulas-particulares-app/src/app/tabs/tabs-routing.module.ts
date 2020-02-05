acda521e39f675b167fe8e24c556fcf3e859f727
naccaratocarolina <naccaratocarolina@poli.ufrj.br>
2020-02-05 14:57:38 -0200

    n sei so pra garantir

--- aulas-particulares-app/src/app/tabs/tabs-routing.module.ts
+++ aulas-particulares-app/src/app/tabs/tabs-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
-import { AuthGuard } from '../guards/Auth/auth.guard';
+import { AuthGuard } from '../guards/Auth/auth.guard'; 
 
 const routes: Routes = [
   {
