0c3823c28b0be3e3ce9e8d518d485525f76fd5ac
naccaratocarolina <naccaratocarolina@poli.ufrj.br>
2020-02-06 21:54:39 -0200

    commit inicial

--- aulas-particulares-app/src/app/tab2/tab2.module.ts
+++ aulas-particulares-app/src/app/tab2/tab2.module.ts
@@ -2,14 +2,16 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { BrMaskerModule } from 'br-mask';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
+    BrMaskerModule,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
   declarations: [Tab2Page]
