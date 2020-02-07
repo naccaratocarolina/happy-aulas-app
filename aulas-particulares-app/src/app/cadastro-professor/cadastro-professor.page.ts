b1956dac5b403f96ceb045903ee823e53eb4a7cb
Carolina Naccarato <naccaratocarolina@gmail.com>
2020-02-07 19:24:53 -0300

    commit inicial

---
+++ aulas-particulares-app/src/app/cadastro-professor/cadastro-professor.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro-professor',
+  templateUrl: './cadastro-professor.page.html',
+  styleUrls: ['./cadastro-professor.page.scss'],
+})
+export class CadastroProfessorPage implements OnInit {
+
+  registerForm: FormGroup;
+
+  constructor(private router: Router, public formbuilder: FormBuilder) {
+    this.registerForm = this.formbuilder.group({
+      nome: [null, [Validators.required]],
+      materia: [null, [Validators.required]],
+      bio: [null, [Validators.required]]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+
+submitForm(form) {
+  console.log(form);
+  console.log(form.value);
+
+  this.router.navigateByUrl('/tabs/tab1'); //redireciona pra home
+}
+
+}
