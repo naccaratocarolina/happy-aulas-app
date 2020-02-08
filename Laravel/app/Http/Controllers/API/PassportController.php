32eea288d15c66b0740a8b4cc44ac3863270d84a
Igor Delfino <igordelfinobarbosa56@gmail.com>
2020-02-08 09:26:42 -0300

    merge_3 X Igor_back_INTEG

--- Laravel/app/Http/Controllers/API/PassportController.php
+++ Laravel/app/Http/Controllers/API/PassportController.php
@@ -19,7 +19,7 @@ class PassportController extends Controller
       $user = new User;
       $user->name = $request->name;
       $user->email = $request->email;
-      $user->password = bcrypt($request->password);
+      $user->password = bcrypt($request->password);//senha encriptada
       $user->c_password = bcrypt($request->c_password);
       $user->save();
       $success['token'] = $user->createToken('MyApp')->accessToken;
