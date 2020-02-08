2810236d446a40fbeb0dd72c001ecbcc405fa7dd
Carolina Naccarato <naccaratocarolina@gmail.com>
2020-02-07 23:07:01 -0300

    + estruturação da pag

--- aulas-particulares-app/src/app/meus-agendamentos/meus-agendamentos.page.ts
+++ aulas-particulares-app/src/app/meus-agendamentos/meus-agendamentos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-meus-agendamentos',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meus-agendamentos.page.scss'],
 })
 export class MeusAgendamentosPage implements OnInit {
+  arrayAgendamentos = [
+    {
+    id: 1,
+    nome_professor: "Nome do Professor",
+    materia: "Matéria",
+    local: "Local",
+    data: "XX/XX/XXXX",
+    hora: "XX:XX"
+    },
+    {
+    id: 2,
+    nome_professor: "Jorge",
+    materia: "Calculo",
+    local: "Zona Norte",
+    data: "7/02/2020",
+    hora: "22:52"
+    },
+    {
+    id: 3,
+    nome_professor: "Maria",
+    materia: "Física",
+    local: "Zona Sul",
+    data: "7/02/2020",
+    hora: "23:01"
+    },
+  ]
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  redirecionaPerfil() {
+  this.router.navigateByUrl('/tabs/tab3'); //redireciona pro perfil
+  }
 }
