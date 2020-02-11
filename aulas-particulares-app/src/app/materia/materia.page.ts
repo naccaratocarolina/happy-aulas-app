c2c40ddf23c1b667887d0d1c36b58eff3ba73a9c
Igor Delfino <igordelfinobarbosa56@gmail.com>
2020-02-11 15:42:05 -0300

    +notifiable

--- aulas-particulares-app/src/app/materia/materia.page.ts
+++ aulas-particulares-app/src/app/materia/materia.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Tab1 } from '../tab1/tab1.page';
+// import { Tab1 } from '../tab1/tab1.page';
 
 @Component({
   selector: 'app-materia',
